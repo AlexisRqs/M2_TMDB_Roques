b2f983c757521f828e079c37e7f38ab98139e993
AlexisRqs <alexisrqs@outlook.fr>
2023-04-06 17:11:00 +0200

    Adding SBF logic & View Model

--- app/src/main/java/com/example/m2_tmdb_roques/SocialBarFragment.kt
+++ app/src/main/java/com/example/m2_tmdb_roques/SocialBarFragment.kt
@@ -1,11 +1,13 @@
 package com.example.m2_tmdb_roques
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
 import com.example.m2_tmdb_roques.databinding.FragmentSocialBarBinding
 
 class SocialBarFragment : Fragment() {
@@ -13,23 +15,72 @@ class SocialBarFragment : Fragment() {
     companion object {
         fun newInstance() = SocialBarFragment()
     }
-
+    private val  LOGTAG = SocialBarFragment::class.simpleName
     private lateinit var binding : FragmentSocialBarBinding
+    private var cn : Int? = Color.LTGRAY
+    private var cs : Int? = Color.RED
 
-    private lateinit var viewModel: SocialBarViewModel
+    // Using the activityViewModels() Kotlin property delegate from the
+    // fragment-ktx artifact to retrieve the ViewModel in the activity scope
+    private val viewModel by activityViewModels<SocialBarViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSocialBarBinding.inflate(inflater, container, false)
-        return binding.root
+        binding = FragmentSocialBarBinding.inflate(inflater)
+        val view = binding.root
+        view.tag = requireArguments().getString("sbfc_view_tag")
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SocialBarViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cn = getContext()?.getColor(R.color.ic_social_normal)
+        cs = getContext()?.getColor(R.color.ic_social_selected)
+
+        Log.d(LOGTAG,"personId=${view.tag}")
+
+        view.tag?.let {
+            val mapkey = it.toString().toInt()
+            Log.d(LOGTAG,"mk=$mapkey")
+
+            // set like button
+            val nblikes = viewModel.nbLikes.getOrElse(mapkey, {0})
+            binding.nbLikeTv.text =nblikes.toString()
+            val likeColor =  if (nblikes > 0 ) cs!! else cn!!
+            binding.likeIv.setColorFilter(likeColor)
+            binding.nbLikeTv.setTextColor(likeColor)
+
+            binding.likeIv.setOnClickListener {
+                viewModel.nbLikes.set(mapkey,viewModel.nbLikes.getOrElse(mapkey, {0}) + 1)
+                binding.nbLikeTv.setText(viewModel.nbLikes[mapkey].toString())
+                binding.likeIv.setColorFilter(cs!!)
+                binding.nbLikeTv.setTextColor(cs!!)
+            }
+
+            // set favorite button
+            val isFavorite = viewModel.isFavorite.getOrElse(mapkey,{false})
+            binding.favoriteIv.setColorFilter(if (isFavorite) cs!! else cn!!)
+            binding.favoriteIv.setOnClickListener {
+                viewModel.isFavorite.set(mapkey, !viewModel.isFavorite.getOrElse(mapkey,{false}))
+                binding.favoriteIv.setColorFilter(if (viewModel.isFavorite[mapkey] == true) cs!! else cn!! )
+            }
+
+            // set share button
+            binding.shareIv.setColorFilter(cn!!)
+            binding.shareIv.setOnClickListener {
+                Log.d(LOGTAG,"shared clicked for id=${mapkey}")
+            }
+        }
+
     }
+
+    /*
+     override fun onActivityCreated(savedInstanceState: Bundle?) {
+         super.onActivityCreated(savedInstanceState)
+         viewModel = ViewModelProvider(this).get(SocialBarViewModel::class.java)
+         // TODO: Use the ViewModel
+     }*/
 
 }
