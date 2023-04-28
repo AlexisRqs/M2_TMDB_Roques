77d042aef41378cb79539f2b2cae1e84900d6d48
AlexisRqs <alexisrqs@outlook.fr>
2023-04-28 17:37:25 +0200

    Corrected build fail, pushing till it's working

--- app/src/main/java/com/example/m2_tmdb_roques/SensorDemoActivity.kt
+++ app/src/main/java/com/example/m2_tmdb_roques/SensorDemoActivity.kt
@@ -1,14 +1,17 @@
 package com.example.m2_tmdb_roques
 
 import android.hardware.Sensor
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.SensorManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.m2_tmdb_roques.databinding.ActivitySensorDemoBinding
 
 class SensorDemoActivity : AppCompatActivity() {
     lateinit var binding: ActivitySensorDemoBinding
     lateinit var sensorListAdapter: SensorListAdapter
+    lateinit var sensorManager: SensorManager
+
     // data model for RV
     private val sensors: ArrayList<Sensor> = ArrayList()
 
@@ -21,7 +24,12 @@ class SensorDemoActivity : AppCompatActivity() {
         binding.sensorListRv.setHasFixedSize(true)
         binding.sensorListRv.layoutManager = LinearLayoutManager(this)
         sensorListAdapter = SensorListAdapter(sensors)
+        binding.sensorListRv.adapter = sensorListAdapter
 
+        // get device's available sensors list
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensors.addAll(sensorManager.getSensorList(Sensor.TYPE_ALL))
+        sensorListAdapter.notifyDataSetChanged()
 
     }
 }
